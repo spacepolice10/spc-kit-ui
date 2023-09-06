e07c1b36511a27e3c4987e3253f49342e4ecef86
spacepolice10 <spcpolice@arbina.com>
2023-09-06 17:14:28 +0600

    chore: update popoverDemo

--- src/overlays/popover/test/PopoverDemo.tsx
+++ src/overlays/popover/test/PopoverDemo.tsx
@@ -2,13 +2,11 @@ import { Popover, Trigger } from "../components/Popover";
 
 export function PopoverDemo() {
   return (
-    <div style={{ position: "relative", width: 200 }}>
-      <Popover offset={4} focusTrapsOnTrigger={true} focusTrapsOnPopover={true}>
-        <Trigger classStyle="border p-2">Popover</Trigger>
-        <div className="bg-blue-400 w-40 h-40">
-          <button onClick={() => alert("YO")}>SOMETHING</button>
-        </div>
-      </Popover>
-    </div>
+    <Popover offset={4} focusTrapsOnTrigger={true} focusTrapsOnPopover={true}>
+      <Trigger classStyle="border p-2">Popover</Trigger>
+      <div className="bg-blue-400 w-40 h-40">
+        <button onClick={() => alert("YO")}>SOMETHING</button>
+      </div>
+    </Popover>
   );
 }
